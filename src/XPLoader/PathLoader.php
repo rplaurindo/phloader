96c74eaa2f9d9d3013d0922335163036ed1c1874
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-07-10 15:49:36 -0300

    first commi

---
+++ src/XPLoader/PathLoader.php
@@ -0,0 +1,33 @@
+<?php
+
+class PathLoader {
+
+    private $className;
+
+    function __construct($className) {
+        $this->className = $className;
+    }
+
+    function load() {
+        $paths = explode(PATH_SEPARATOR, get_include_path());
+        $directoryIterator = new GraphIte\Directory($paths);
+
+        foreach ($directoryIterator as $path) {
+            $namespacedPath =  str_replace('\\', DIRECTORY_SEPARATOR, $this->className)  . '.php';
+            $absolutePath = $path . DIRECTORY_SEPARATOR . $namespacedPath;
+            if (file_exists($absolutePath)) {
+                include $namespacedPath;
+                break;
+            } else {
+                $fileName = basename($this->className)  . '.php';
+                $absolutePath = $path . DIRECTORY_SEPARATOR . $fileName;
+
+                if (file_exists($absolutePath)) {
+                    include $absolutePath;
+                    break;
+                }
+            }
+        }
+    }
+
+}
